c23cf11252415587094f6487b2a2000db48fd9e7
Jack Geiger <jpgeiger6789@gmail.com>
2021-11-23 18:55:28 -0500

    Help!

--- Chapter 1/Problem 1.6.py
+++ Chapter 1/Problem 1.6.py
@@ -106,8 +106,10 @@ a is the offset from the origin along the x axis to the center of the bell curve
                                                                                                             
     R = u^2                                                                                                 <<This will differentiate into a term I know how to integrate (it will go to 0)
     dT = A * e^(-u^2) * du                                                                                  <<this looks like our old friend D(x)
-    dT = sqrt(λ/pi) * e^(-λ[x-a]^2) * dx                                                                    <<λ = 1/(2σ^2)
-    dT = sqrt(λ/pi) * e^(-[x-a]^2/(2σ^2)) * dx                                                              <<This is our original form of the equation which integrates to equal D(x).
+    dT = sqrt(λ/pi) * e^(-λ[x-a]^2) * dx                                                                    <<A = sqrt(λ/pi)
+    dT = sqrt(λ/pi) * e^(-[x-a]^2/(2σ^2)) * dx                                                              <<λ = 1/(2σ^2)
+        This is our original form of the cumulative distribution function.
+        when integrated from x'=-∞ to x'=x, this is equal to D(x).
     
     dR = 2u * du
     T = ∫ dT 
